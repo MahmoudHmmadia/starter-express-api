6d4e6eb86ecbce487999e3e8961dbb334bd4e28a
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-10-05 09:37:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import { PeerServer, ExpressPeerServer } from "peer";
+import cors from "cors";
+// const peerServer = PeerServer({
+//   path: "/",
+//   port: 3001,
+// });
+const app = express();
+app.use(cors({ origin: true, credentials: true, optionsSuccessStatus: 200 }));
+app.set("view engine", "ejs");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: true,
+    methods: ["GET", "POST"],
+  },
+});
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("room");
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (rId, uId) => {
+    socket.join(rId);
+    socket.to(rId).emit("user-connected", uId);
+    socket.on("disconnect", () => {
+      socket.to(rId).emit("user-disconnected", uId);
+    });
+  });
+});
+
+const expressPeerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: "/",
+  corsOptions: {
+    origin: true,
+  },
+});
+app.use(expressPeerServer);
+server.listen(3000, () => {});
